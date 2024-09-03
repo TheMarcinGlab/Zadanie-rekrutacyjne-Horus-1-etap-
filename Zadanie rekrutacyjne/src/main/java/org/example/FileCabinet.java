6369540bfc0604706148f8fbe85199312bde614e
Marcin Głąb <marcinglabppx@gmail.com>
2024-09-03 18:41:02 +0200

    implementacja metody findFoldersBySize()

--- Zadanie rekrutacyjne/src/main/java/org/example/FileCabinet.java
+++ Zadanie rekrutacyjne/src/main/java/org/example/FileCabinet.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -13,30 +14,43 @@ public class FileCabinet implements Cabinet {
 
     @Override
     public Optional<Folder> findFolderByName(String name) {
-       for(Folder folder : folders){
-           if(folder.getName().equals(name)){
-               return Optional.of(folder);
-           }
-           if(folder instanceof MultiFolder){
-               for(Folder podFolder : ((MultiFolder) folder).getFolders()){
-                   Optional<Folder> znalezionyFolder = new FileCabinet(Arrays.asList(podFolder)).findFolderByName(name);
-                   if(znalezionyFolder.isPresent()){
-                       return znalezionyFolder;
-                   }
-               }
-           }
+        for (Folder folder : folders) {
+            if (folder.getName().equals(name)) {
+                return Optional.of(folder);
+            }
+            if (folder instanceof MultiFolder) {
+                for (Folder podFolder : ((MultiFolder) folder).getFolders()) {
+                    Optional<Folder> znalezionyFolder = new FileCabinet(Arrays.asList(podFolder)).findFolderByName(name);
+                    if (znalezionyFolder.isPresent()) {
+                        return znalezionyFolder;
+                    }
+                }
+            }
 
-       }
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        return null;
+        List<Folder> wynik = new ArrayList<>();
+
+        for (Folder folder : folders) {
+            if (folder.getSize().equals(size)) {
+                wynik.add(folder);
+            }
+            if (folder instanceof MultiFolder) {
+                wynik.addAll(new FileCabinet(((MultiFolder) folder).getFolders()).findFoldersBySize(size));
+            }
+
+        }
+        return wynik;
     }
 
     @Override
     public int count() {
+
         return 0;
     }
+
 }
